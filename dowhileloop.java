a8bf4c1a0fbc210ded73428f97af330f27d1f2c3
sdhariharan <sdhariharan7@gmail.com>
2025-11-14 22:04:03 +0530

    do while loop

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("Value: " + i);
+            i++;    // increment
+        } while(i <= 5);
+    }
+}
+
