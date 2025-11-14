3a9185ed3329d914d6ff7fb2d0d1607b4599ef0c
sdhariharan <sdhariharan7@gmail.com>
2025-11-14 21:58:12 +0530

    If-Else

---
+++ Ifelse.java
@@ -0,0 +1,12 @@
+public class Ifelse{
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if(age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+    }
+}
