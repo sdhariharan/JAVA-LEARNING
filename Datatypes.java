51861432157087da91a5e06beb193e0220293790
sdhariharan <sdhariharan7@gmail.com>
2025-11-14 21:36:27 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+public class Datatypes {
+    public static void main(String[] args) {
+
+        // Primitive Datatypes
+        byte a = 10;
+        short b = 200;
+        int age = 20;
+        long population = 7800000000L;
+
+        float price = 45.50f;
+        double weight = 68.89;
+
+        char grade = 'A';
+        boolean isStudent = true;
+
+        // Non-Primitive Datatype
+        String name = "Hariharan";
+
+        // Output
+        System.out.println("Byte: " + a);
+        System.out.println("Short: " + b);
+        System.out.println("Age: " + age);
+        System.out.println("Population: " + population);
+        System.out.println("Price: " + price);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Name: " + name);
+    }
+}
+
+
