b9051a212a5863f1fa80ed310d13ea5016a7916f
sdhariharan <sdhariharan7@gmail.com>
2025-11-14 22:13:56 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("You entered:");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        sc.close();
+    }
+}
+
