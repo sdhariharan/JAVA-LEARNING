e3c7b9a94c86fb2d2d0b17705f302fe5898b610b
sdhariharan <sdhariharan7@gmail.com>
2025-11-14 22:25:13 +0530

    Fibonacci

---
+++ Fibbonacci.java
@@ -0,0 +1,14 @@
+public class Fibbonacci{
+    public static void main(String[] args) {
+
+        int n = 10, a = 0, b = 1;
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
+
