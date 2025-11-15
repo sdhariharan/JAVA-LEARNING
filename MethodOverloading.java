090c80c59836a27842cec9ce2e261038a94435a1
sdhariharan <sdhariharan7@gmail.com>
2025-11-15 11:54:10 +0530

    Method Overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,25 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class MethodOverloading{
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.add(5, 10));
+        System.out.println(calc.add(5.5, 3.2));
+        System.out.println(calc.add(1, 2, 3));
+    }
+}
+
+
