db912f433fe0b1b7f47dbdcd831863e5425f3159
sdhariharan <sdhariharan7@gmail.com>
2025-11-16 17:21:33 +0530

    String Builder Methods

---
+++ StringBuilders.java
@@ -0,0 +1,59 @@
+public class StringBuilders{
+    public static void main(String[] args) {
+
+        // 1. Creating StringBuilder
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println("Initial: " + sb);
+
+        // 2. append()
+        sb.append(" World");
+        System.out.println("After append: " + sb);
+
+        // 3. insert()
+        sb.insert(5, " Java");
+        System.out.println("After insert: " + sb);
+
+        // 4. replace()
+        sb.replace(6, 10, "C++");
+        System.out.println("After replace: " + sb);
+
+        // 5. delete()
+        sb.delete(6, 10);
+        System.out.println("After delete: " + sb);
+
+        // 6. deleteCharAt()
+        sb.deleteCharAt(5);
+        System.out.println("After deleteCharAt: " + sb);
+
+        // 7. setCharAt()
+        sb.setCharAt(0, 'h');
+        System.out.println("After setCharAt: " + sb);
+
+        // 8. charAt()
+        char ch = sb.charAt(1);
+        System.out.println("charAt(1): " + ch);
+
+        // 9. length()
+        System.out.println("Length: " + sb.length());
+
+        // 10. capacity()
+        System.out.println("Capacity: " + sb.capacity());
+
+        // 11. ensureCapacity()
+        sb.ensureCapacity(40);
+        System.out.println("After ensureCapacity(40), capacity: " + sb.capacity());
+
+        // 12. substring()
+        String sub = sb.substring(0, 5);
+        System.out.println("substring(0,5): " + sub);
+
+        // 13. setLength()
+        sb.setLength(4);     // Trims the string
+        System.out.println("After setLength(4): " + sb);
+
+        // 14. reverse()
+        sb.reverse();
+        System.out.println("After reverse: " + sb);
+    }
+}
+
